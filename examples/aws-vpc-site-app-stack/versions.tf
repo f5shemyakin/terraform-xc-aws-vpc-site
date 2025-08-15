d4e1f69c930c9df0a635da95149bd83ec99286a0
Alex Shemyakin <a.shemyakin@f5.com>
2025-08-15 23:04:29 +0200

    update versions

--- examples/aws-vpc-site-app-stack/versions.tf
+++ examples/aws-vpc-site-app-stack/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     volterra = {
       source  = "volterraedge/volterra"
-      version = ">= 0.11.26"
+      version = "= 0.11.44"
     }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 4.65.0"
+      version = ">= 6.9.0"
     }
   }
 }
